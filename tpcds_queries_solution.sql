b59bd4305044bba885c3f9b34925816a93056d02
sko71 <sanjay_kotecha1@yahoo.com>
2018-02-13 17:55:08 -0500

    Add files via upload

---
+++ tpcds_queries_solution.sql
@@ -0,0 +1,299 @@
+-- These tpc-ds queries have been edited slightly from the output of dsdqgen program.
+
+-- start query 34 in stream 0 using template query34.tpl
+-- Display all customers with specific buy potentials and whose dependent count to vehicle count ratio is larger
+-- than 1.2, who in three consecutive years made purchases with between 15 and 20 items in the beginning or the
+-- end of each month in stores located in 8 counties.
+select c_last_name
+       ,c_first_name
+       ,c_salutation
+       ,c_preferred_cust_flag
+       ,ss_ticket_number
+       ,cnt from
+   (select ss_ticket_number
+          ,ss_customer_sk
+          ,count(*) cnt
+    from store_sales,date_dim,store,household_demographics
+    where store_sales.ss_sold_date_sk = date_dim.d_date_sk
+    and store_sales.ss_store_sk = store.s_store_sk
+    and store_sales.ss_hdemo_sk = household_demographics.hd_demo_sk
+    and (date_dim.d_dom between 1 and 3 or date_dim.d_dom between 25 and 28)
+    and (household_demographics.hd_buy_potential = '501-1000' or
+         household_demographics.hd_buy_potential = '0-500')
+    and household_demographics.hd_vehicle_count > 0
+    and (case when household_demographics.hd_vehicle_count > 0
+	then household_demographics.hd_dep_count/ household_demographics.hd_vehicle_count
+	else null
+	end)  > 1.2
+    and date_dim.d_year in (1998,1998+1,1998+2)
+    and store.s_county in ('Williamson County','Ziebach County','Ziebach County','Williamson County',
+                           'Ziebach County','Ziebach County','Ziebach County','Ziebach County')
+    group by ss_ticket_number,ss_customer_sk) dn,customer
+    where ss_customer_sk = c_customer_sk
+      and cnt between 15 and 20
+    order by c_last_name,c_first_name,c_salutation,c_preferred_cust_flag desc, ss_ticket_number;
+
+
+-- start query 4 in stream 0 using template query4.tpl
+-- Find customers who spend more money via catalog than in stores. Identify preferred customers and their
+-- country of origin.
+    with year_total as (
+     select c_customer_id customer_id
+           ,c_first_name customer_first_name
+           ,c_last_name customer_last_name
+           ,c_preferred_cust_flag customer_preferred_cust_flag
+           ,c_birth_country customer_birth_country
+           ,c_login customer_login
+           ,c_email_address customer_email_address
+           ,d_year dyear
+           ,sum(((ss_ext_list_price-ss_ext_wholesale_cost-ss_ext_discount_amt)+ss_ext_sales_price)/2) year_total
+           ,'s' sale_type
+     from customer_all
+         ,store_sales_dist_sort
+         ,date_dim
+     where c_customer_sk = ss_customer_sk
+       and ss_sold_date_sk = d_date_sk
+     group by c_customer_id
+             ,c_first_name
+             ,c_last_name
+             ,c_preferred_cust_flag
+             ,c_birth_country
+             ,c_login
+             ,c_email_address
+             ,d_year
+     union all
+     select c_customer_id customer_id
+           ,c_first_name customer_first_name
+           ,c_last_name customer_last_name
+           ,c_preferred_cust_flag customer_preferred_cust_flag
+           ,c_birth_country customer_birth_country
+           ,c_login customer_login
+           ,c_email_address customer_email_address
+           ,d_year dyear
+           ,sum((((cs_ext_list_price-cs_ext_wholesale_cost-cs_ext_discount_amt)+cs_ext_sales_price)/2) ) year_total
+           ,'c' sale_type
+     from customer_all
+         ,catalog_sales
+         ,date_dim
+     where c_customer_sk = cs_bill_customer_sk
+       and cs_sold_date_sk = d_date_sk
+     group by c_customer_id
+             ,c_first_name
+             ,c_last_name
+             ,c_preferred_cust_flag
+             ,c_birth_country
+             ,c_login
+             ,c_email_address
+             ,d_year
+    union all
+     select c_customer_id customer_id
+           ,c_first_name customer_first_name
+           ,c_last_name customer_last_name
+           ,c_preferred_cust_flag customer_preferred_cust_flag
+           ,c_birth_country customer_birth_country
+           ,c_login customer_login
+           ,c_email_address customer_email_address
+           ,d_year dyear
+           ,sum((((ws_ext_list_price-ws_ext_wholesale_cost-ws_ext_discount_amt)+ws_ext_sales_price)/2) ) year_total
+           ,'w' sale_type
+     from customer_all
+         ,web_sales
+         ,date_dim
+     where c_customer_sk = ws_bill_customer_sk
+       and ws_sold_date_sk = d_date_sk
+     group by c_customer_id
+             ,c_first_name
+             ,c_last_name
+             ,c_preferred_cust_flag
+             ,c_birth_country
+             ,c_login
+             ,c_email_address
+             ,d_year
+             )
+      select
+                      t_s_secyear.customer_id
+                     ,t_s_secyear.customer_first_name
+                     ,t_s_secyear.customer_last_name
+                     ,t_s_secyear.customer_birth_country
+     from year_total t_s_firstyear
+         ,year_total t_s_secyear
+         ,year_total t_c_firstyear
+         ,year_total t_c_secyear
+         ,year_total t_w_firstyear
+         ,year_total t_w_secyear
+     where t_s_secyear.customer_id = t_s_firstyear.customer_id
+       and t_s_firstyear.customer_id = t_c_secyear.customer_id
+       and t_s_firstyear.customer_id = t_c_firstyear.customer_id
+       and t_s_firstyear.customer_id = t_w_firstyear.customer_id
+       and t_s_firstyear.customer_id = t_w_secyear.customer_id
+       and t_s_firstyear.sale_type = 's'
+       and t_c_firstyear.sale_type = 'c'
+       and t_w_firstyear.sale_type = 'w'
+       and t_s_secyear.sale_type = 's'
+       and t_c_secyear.sale_type = 'c'
+       and t_w_secyear.sale_type = 'w'
+       and t_s_firstyear.dyear =  1999
+       and t_s_secyear.dyear = 1999+1
+       and t_c_firstyear.dyear =  1999
+       and t_c_secyear.dyear =  1999+1
+       and t_w_firstyear.dyear = 1999
+       and t_w_secyear.dyear = 1999+1
+       and t_s_firstyear.year_total > 0
+       and t_c_firstyear.year_total > 0
+       and t_w_firstyear.year_total > 0
+       and case when t_c_firstyear.year_total > 0 then t_c_secyear.year_total / t_c_firstyear.year_total else null end
+               > case when t_s_firstyear.year_total > 0 then t_s_secyear.year_total / t_s_firstyear.year_total else null end
+       and case when t_c_firstyear.year_total > 0 then t_c_secyear.year_total / t_c_firstyear.year_total else null end
+               > case when t_w_firstyear.year_total > 0 then t_w_secyear.year_total / t_w_firstyear.year_total else null end
+     order by t_s_secyear.customer_id
+             ,t_s_secyear.customer_first_name
+             ,t_s_secyear.customer_last_name
+             ,t_s_secyear.customer_birth_country
+    ;
+
+-- start query 11 in stream 0 using template query11.tpl
+--Find customers whose increase in spending was large over the web than in stores this year compared to last
+--year.
+
+with year_total as (
+ select c_customer_id customer_id
+       ,c_first_name customer_first_name
+       ,c_last_name customer_last_name
+       ,c_preferred_cust_flag customer_preferred_cust_flag
+       ,c_birth_country customer_birth_country
+       ,c_login customer_login
+       ,c_email_address customer_email_address
+       ,d_year dyear
+       ,sum(ss_ext_list_price-ss_ext_discount_amt) year_total
+       ,'s' sale_type
+ from customer
+     ,store_sales
+     ,date_dim
+ where c_customer_sk = ss_customer_sk
+   and ss_sold_date_sk = d_date_sk
+ group by c_customer_id
+         ,c_first_name
+         ,c_last_name
+         ,c_preferred_cust_flag
+         ,c_birth_country
+         ,c_login
+         ,c_email_address
+         ,d_year
+ union all
+ select c_customer_id customer_id
+       ,c_first_name customer_first_name
+       ,c_last_name customer_last_name
+       ,c_preferred_cust_flag customer_preferred_cust_flag
+       ,c_birth_country customer_birth_country
+       ,c_login customer_login
+       ,c_email_address customer_email_address
+       ,d_year dyear
+       ,sum(ws_ext_list_price-ws_ext_discount_amt) year_total
+       ,'w' sale_type
+ from customer
+     ,web_sales
+     ,date_dim
+ where c_customer_sk = ws_bill_customer_sk
+   and ws_sold_date_sk = d_date_sk
+ group by c_customer_id
+         ,c_first_name
+         ,c_last_name
+         ,c_preferred_cust_flag
+         ,c_birth_country
+         ,c_login
+         ,c_email_address
+         ,d_year
+         )
+  select
+                  t_s_secyear.customer_id
+                 ,t_s_secyear.customer_first_name
+                 ,t_s_secyear.customer_last_name
+                 ,t_s_secyear.customer_birth_country
+ from year_total t_s_firstyear
+     ,year_total t_s_secyear
+     ,year_total t_w_firstyear
+     ,year_total t_w_secyear
+ where t_s_secyear.customer_id = t_s_firstyear.customer_id
+         and t_s_firstyear.customer_id = t_w_secyear.customer_id
+         and t_s_firstyear.customer_id = t_w_firstyear.customer_id
+         and t_s_firstyear.sale_type = 's'
+         and t_w_firstyear.sale_type = 'w'
+         and t_s_secyear.sale_type = 's'
+         and t_w_secyear.sale_type = 'w'
+         and t_s_firstyear.dyear = 1998
+         and t_s_secyear.dyear = 1998+1
+         and t_w_firstyear.dyear = 1998
+         and t_w_secyear.dyear = 1998+1
+         and t_s_firstyear.year_total > 0
+         and t_w_firstyear.year_total > 0
+         and case when t_w_firstyear.year_total > 0 then t_w_secyear.year_total / t_w_firstyear.year_total else 0.0 end
+             > case when t_s_firstyear.year_total > 0 then t_s_secyear.year_total / t_s_firstyear.year_total else 0.0 end
+ order by t_s_secyear.customer_id
+         ,t_s_secyear.customer_first_name
+         ,t_s_secyear.customer_last_name
+         ,t_s_secyear.customer_birth_country
+;
+
+-- query 57
+--Find the item brands and categories for each call center and their monthly sales figures for a specified year,
+-- where the monthly sales figure deviated more than 10% of the average monthly sales for the year, sorted by
+-- deviation and call center. Report the sales deviation from the previous and following month.
+  with v1 as(
+    select i_category, i_brand, cc_name, d_year, d_moy,
+          sum(cs_sales_price) sum_sales,
+          avg(sum(cs_sales_price)) over
+            (partition by i_category, i_brand,
+                       cc_name, d_year)
+            avg_monthly_sales,
+          rank() over
+            (partition by i_category, i_brand,
+                       cc_name
+             order by d_year, d_moy) rn
+    from item_dist, catalog_sales_dist_sort, date_dim_sort, call_center_all
+    where cs_item_sk = i_item_sk and
+         cs_sold_date_sk = d_date_sk and
+         cc_call_center_sk= cs_call_center_sk; and
+         (
+           d_year = 1999 or
+           ( d_year = 1999-1 and d_moy =12) or
+           ( d_year = 1999+1 and d_moy =1)
+         )
+    group by i_category, i_brand,
+            cc_name , d_year, d_moy),
+  v2 as(
+    select v1.i_category ,v1.d_year, v1.d_moy ,v1.avg_monthly_sales
+          ,v1.sum_sales, v1_lag.sum_sales psum, v1_lead.sum_sales nsum
+    from v1, v1 v1_lag, v1 v1_lead
+    where v1.i_category = v1_lag.i_category and
+         v1.i_category = v1_lead.i_category and
+         v1.i_brand = v1_lag.i_brand and
+         v1.i_brand = v1_lead.i_brand and
+         v1.cc_name = v1_lag.cc_name and
+         v1.cc_name = v1_lead.cc_name and
+         v1.rn = v1_lag.rn + 1 and
+         v1.rn = v1_lead.rn - 1)
+  select  *
+  from v2
+  where  d_year = 1999 and
+          avg_monthly_sales > 0 and
+          case when avg_monthly_sales > 0 then abs(sum_sales - avg_monthly_sales) / avg_monthly_sales else null end > 0.1
+  order by sum_sales - avg_monthly_sales, 3
+  limit 100;
+
+-- query 57 (edited)
+select i_category, i_brand, cc_name, d_year, d_moy,
+        sum(cs_sales_price) sum_sales,
+        avg(sum(cs_sales_price)) over
+          (partition by i_category, i_brand,
+                     cc_name, d_year)
+          avg_monthly_sales,
+        rank() over
+          (partition by i_category, i_brand,
+                     cc_name
+           order by d_year, d_moy) rn
+from item_all, catalog_sales, date_dim_all , call_center_all
+where cs_item_sk = i_item_sk and
+       cs_sold_date_sk = d_date_sk and
+       cc_call_center_sk= cs_call_center_sk
+group by i_category, i_brand,
+          cc_name , d_year, d_moy;
