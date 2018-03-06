cc5e3813d90a603d06eb0a99f78eb1ebb99e7165
sko71 <sanjay_kotecha1@yahoo.com>
2018-03-06 15:49:14 -0500

    Add files via upload

--- tpcdsddl_solution.sql
+++ tpcdsddl_solution.sql
@@ -186,6 +186,32 @@ CREATE TABLE public.store_all (
 	s_tax_percentage numeric(5,2)
 ) diststyle all;
 
+CREATE TABLE public.store_sales_dist (
+	ss_sold_date_sk int8 sortkey,
+	ss_sold_time_sk int8,
+	ss_item_sk int8 NOT NULL,
+	ss_customer_sk int8,
+	ss_cdemo_sk int8,
+	ss_hdemo_sk int8,
+	ss_addr_sk int8,
+	ss_store_sk int8 distkey,
+	ss_promo_sk int8,
+	ss_ticket_number int8 NOT NULL,
+	ss_quantity int8,
+	ss_wholesale_cost numeric(7,2),
+	ss_list_price numeric(7,2),
+	ss_sales_price numeric(7,2),
+	ss_ext_discount_amt numeric(7,2),
+	ss_ext_sales_price numeric(7,2),
+	ss_ext_wholesale_cost numeric(7,2),
+	ss_ext_list_price numeric(7,2),
+	ss_ext_tax numeric(7,2),
+	ss_coupon_amt numeric(7,2),
+	ss_net_paid numeric(7,2),
+	ss_net_paid_inc_tax numeric(7,2),
+	ss_net_profit numeric(7,2));
+
+
 CREATE TABLE public.store_sales_dist_sort (
 	ss_sold_date_sk int8 sortkey,
 	ss_sold_time_sk int8,
